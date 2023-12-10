441c0fe1d8270859431f5521d05e0fb76198e16c
P-Yubing <beingyb2244@gmail.com>
2023-12-10 14:41:29 +0900

    first commit

---
+++ TestProject/src/main/java/ddit/or/kr/TestProjectApplication.java
@@ -0,0 +1,13 @@
+package ddit.or.kr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjectApplication.class, args);
+	}
+
+}
